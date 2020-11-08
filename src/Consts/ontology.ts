5e0d2c7eea8553ac2be3356d23c56f98f89bdec1
Ben <hason.ben2@gmail.com>
2020-11-08 10:24:33 +0200

    fixed ontology

--- src/Consts/ontology.ts
+++ src/Consts/ontology.ts
@@ -1,12 +1,12 @@
 export const entityTypes = [
   { type: "Person", id: "1" },
-  { type: "פלאפון", id: "2" },
+  { type: "Phone", id: "2" },
 ];
 
 export const entityTypeOntology = [
   {
     id: "1",
-    type: "אדם",
+    type: "Person",
     properties: {
       must: [{ age: "number" }, ],
       optional: [{ birthDay: "date" }, { eyeColor: "text" },{ name: "text" }],
@@ -14,7 +14,7 @@ export const entityTypeOntology = [
   },
   {
     id: "2",
-    type: "פלאפון",
+    type: "Phone",
     properties: {
       must: [{ phoneNumber: "text" }, { name: "text" }],
       optional: [{ birthDay: "date" }],
